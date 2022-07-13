c77c1e0d8cb6d73e2db0547c3c46520340dcebc4
lzyjin <1247y.j@gmail.com>
2022-07-13 22:43:54 +0900

    끝말잇기 - ing

--- src/views/endTalk/index.vue
+++ src/views/endTalk/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
       <h1> <span style="cursor: pointer" @click="$router.go(-1)">&lt;</span>&nbsp;&nbsp; 끝말 잇기</h1>
+
+    <div>
+      <p class="score-time"><span>점수: {{ score }}</span><span>시간: {{ time }}</span></p>
+      <p>단어: {{ prevText }}</p>
+      <div>
+        <span>{{ myTurn ? '나' : '컴퓨터' }}: </span>
+        <input type="text" v-model="currText" v-on:keyup.enter="send">
+        <button v-on:click="send">입력</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,13 +28,55 @@ export default {
   },
   data() {
     return {
+      myTurn: true,
+      score: 0,
+      time: 10,
+      prevText: '강아지',
+      currText: '',
     };
   },
   methods: {
+    send() {
+      setInterval(() => {
+        // setTimeout(() => {
+        //   if (this.time > 0) {
+        //     this.time -= 1;
+        //   } else {
+        //     // interval 클리어 하고싶음
+        //     this.myTurn = !this.myTurn;
+        //     this.time = 10;
+        //   }
+        //   console.log(this.time);
+        // }, 1000);
+        if (this.time > 0) {
+          this.time -= 1;
+        } else {
+          // interval 클리어 하고싶음
+          this.myTurn = !this.myTurn;
+          this.time = 10;
+        }
+        console.log(this.time);
+      }, 1000);
+      if (this.prevText.slice(-1) === this.currText.slice(0, 1)) {
+        if (this.myTurn) {
+          alert('잘했어요!');
+          this.score += 10;
+        }
+        this.prevText = this.currText;
+        this.currText = '';
+        this.myTurn = !this.myTurn;
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
+  .score-time {
 
+  }
+  .score-time span {
+    display: inline-block;
+    margin-right: 20px;
+  }
 </style>
